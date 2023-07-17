afeabd0f36ef537c1973f52cf021d0561ae12e9d
0123sandeepkumar <speedan0123@gmail.com>
2023-07-17 22:58:34 +0530

    imp hooks

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ComponentA1 from './hooks/useState/ComponentA1.jsx';
+
+export default function App() {
+    return (
+    <>
+    <div>Hello World</div>
+    <ComponentA1/>
+    </>
+    );
+    }
+    
