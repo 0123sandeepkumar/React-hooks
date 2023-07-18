3cf55b191e9c73c722efdda664c850f806397b43
0123sandeepkumar <speedan0123@gmail.com>
2023-07-18 23:28:24 +0530

    new component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import ComponentA1 from './hooks/useState/ComponentA1.jsx';
+import ComponentA2 from './hooks/useState/ComponentA2.jsx';
 
 export default function App() {
     return (
     <>
     <div>Hello World</div>
     <ComponentA1/>
+    <br/>
+    <br/>
+    <ComponentA2/>
     </>
     );
     }
