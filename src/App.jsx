3017567d9b9550dd52d8fdeabf2a7475e3c56ccf
0123sandeepkumar <speedan0123@gmail.com>
2023-07-19 07:53:14 +0530

    usestate wrap up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import ComponentA1 from './hooks/useState/ComponentA1.jsx';
 import ComponentA2 from './hooks/useState/ComponentA2.jsx';
+import ComponentA3 from './hooks/useState/ComponentA3.jsx';
+import ComponentA4 from './hooks/useState/ComponentA4.jsx';
+import ComponentA5 from './hooks/useState/ComponentA5.jsx';
+import ComponentA6 from './hooks/useState/ComponentA6.jsx';
+import ComponentA7 from './hooks/useState/ComponentA7.jsx';
+import ComponentA8 from './hooks/useState/ComponentA8.jsx';
+import ComponentA9 from './hooks/useState/ComponentA9.jsx';
+import ComponentA10 from './hooks/useState/ComponentA10.jsx';
 
 export default function App() {
     return (
@@ -9,6 +17,30 @@ export default function App() {
     <br/>
     <br/>
     <ComponentA2/>
+    <br />
+    <br />
+    <ComponentA3/>
+    <br />
+    <br />
+    <ComponentA4/>
+    <br />
+    <br />
+    <ComponentA5/>
+    <br />
+    <br />
+    <ComponentA6/>
+    <br />
+    <br />
+    <ComponentA7/>
+    <br />
+    <br />
+    <ComponentA8/>
+    <br />
+    <br />
+    <ComponentA9/>
+    <br />
+    <br />
+    <ComponentA10/>
     </>
     );
     }
